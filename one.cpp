eab725c2b95ef6c9d3dd058c9ecdedfa57b48d71
manav <mmalhotra_be21@thapar.edu>
2022-11-09 12:58:12 +0530

    initial commit

---
+++ one.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<list>
+#include<vector>
+#include<map>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    cout<<"Hello world"<<endl;
+    return 0;
+}
